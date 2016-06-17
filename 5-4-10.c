a51fa378ffe8a09b8b4235d55106708b8ab0f0ea
jivoi <don.eugen@gmail.com>
2016-06-17 13:12:12 +0300

    tmpfs

---
+++ 5-4-10.c
@@ -0,0 +1,43 @@
+// Задача на именованные каналы.
+
+// Напишите программу, которая создает два именованных канала - /home/box/in.fifo и /home/box/out.fifo
+
+// Пусть программа читает in.fifo и все прочитанное записывает в out.fifo.
+
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#define MAX_BUF 2048
+
+int main()
+{
+    char * outfifo = "/home/box/out.fifo";
+    char * infifo = "/home/box/in.fifo";
+
+    /* create the FIFO (named pipe) */
+    mkfifo(infifo, 0666);
+    mkfifo(outfifo, 0666);
+
+    int outfd = open(outfifo, O_WRONLY);
+    perror("Error after outfd open: ");
+    int infd = open(infifo, O_RDONLY | O_NONBLOCK);
+    perror("Error after infd open: ");
+
+    char buf[MAX_BUF];
+    for (;;)
+    {
+        int got = read(infd, buf, MAX_BUF);
+        write(outfd, buf, got);
+    }
+
+    close(outfd);
+    close(infd);
+
+    /* remove the FIFO */
+    unlink(infifo);
+    unlink(outfifo);
+
+    return 0;
+}
---
+++ 5-4-11.c
@@ -0,0 +1,35 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdio.h>
+
+void child(int socket) {
+    const char hello[] = "I am your child, Wherever you go, You take me too, Whatever I knowm I learned from you";
+    write(socket, hello, sizeof(hello));
+    while (1);
+}
+
+void parent(int socket) {
+    char buf[1024];
+    int n = read(socket, buf, sizeof(buf));
+    printf("parent received '%.*s'\n", n, buf);
+    while (1);
+}
+
+int main(int argc, char **argv) {
+    int fd[2];
+    static const int parentsocket = 0;
+    static const int childsocket = 1;
+    pid_t pid;
+
+    socketpair(PF_LOCAL, SOCK_STREAM, 0, fd);
+    pid = fork();
+    if (pid == 0) {
+        close(fd[parentsocket]);
+        child(fd[childsocket]);
+    } else {
+        close(fd[childsocket]);
+        parent(fd[parentsocket]);
+    }
+    return 0;
+}
